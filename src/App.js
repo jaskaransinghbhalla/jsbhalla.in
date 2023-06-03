02bce34b577154afa1bab5c407ead06e481d7d6a
Jaskaran <jaskaranofficial15@gmail.com>
2023-06-04 01:28:48 +0530

    tailwind

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import Education from "./components/Education";
 import Courses from "./components/Courses";
 import Media from "./components/Media";
 import Skills from "./components/Skills";
+import Navbar from "./components/Skills";
 function App() {
   return (
     <div>
       <body>
-        Name : Jaskaran Singh Bhalla
+        <div className="mx-auto bg-black text-white p-2 content-between">
+          <h1 className="text-3xl font-bold">
+            Jaskaran Singh Bhalla
+          </h1>
+        </div>
+
         <br />
-        Programme : B.Tech in Textile Technology,
         IIT Delhi
-        <Education/>
-        <Skills/>
-        <Courses/>
-        <Media/>
+        <Navbar />
+        <Education />
+        <Skills />
+        <Courses />
+        <Media />
       </body>
     </div>
   );
