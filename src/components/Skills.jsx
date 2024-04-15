508aee1d40c28341abe31a3cb4655eac5c26baf1
Jaskaran <jaskaranofficial15@gmail.com>
2024-04-15 09:01:04 +0530

    Updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Skills() {
     return <div className="my-6 text-justify">
         <div className="text-2xl font-bold">My Skills</div>
@@ -29,8 +31,26 @@ export default function Skills() {
                 <li className="my-1">Java</li>
             </ul>
         </div>
+
         <div className="mt-4 py-2">
             I am currently learning about Blockchain and Smart Contracts, Synthesis of Digital Systems, Venture Financing and Teaming, Next.js and Artifical Intelligence.
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 mx-4 my-3 font-medium ">
+            <ul className="list-disc">
+                <li className="my-1"><Link to="https://jaskaransinghbhalla.notion.site/Solidity-d6f2337e94d541a98baac56a1d431e98">Solidity</Link></li>
+                <li className="my-1"><Link to="https://thirdweb.com/">Thirdweb</Link></li>
+            </ul>
+            <ul className="list-disc">
+                <li className="my-1"><Link to="https://archive.trufflesuite.com/ganache/">Ganache</Link></li>
+                <li className="my-1"><Link to="https://ethereum.org/en/">Etherum</Link></li>
+            </ul>
+            <ul className="list-disc">
+                <li className="my-1"><Link to="https://archive.trufflesuite.com/">Truffle</Link></li>
+                <li className="my-1"><Link to="https://hardhat.org/">Hardhat</Link></li>
+            </ul>
+            <ul className="list-disc">
+                <li className="my-1"><Link to="https://www.openzeppelin.com/">Openzipline</Link></li>
+            </ul>
         </div>
     </div>
 }
