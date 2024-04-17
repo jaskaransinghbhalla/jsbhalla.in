11296cfb2d3cf03ec01e5a016128d216f4450281
Jaskaran <jaskaranofficial15@gmail.com>
2024-04-17 08:18:58 +0530

    Updated

--- src/pages/Watchlog.jsx
+++ src/pages/Watchlog.jsx
@@ -1,33 +1,54 @@
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import { useEffect } from 'react';
-const content = [
-    {
-        title: "Amar Singh Chamikla",
-        genre: "Biopic, Drama, Musical",
-        director: "Imtiaz Ali",
-        watchDate: "13, April 2024",
-        platform: "Netflix",
-    },
-    {
-        title: "Murder Mubarak",
-        genre: "Comedy, Mystery, Horror",
-        director: "Homi Adajania",
-        watchDate: "06-07, April 2024",
-        platform: "Netflix",
-    },
-    {
-        title: "Farzi",
-        genre: "Black comedy, Crime thriller",
-        director: "Raj & DK",
-        watchDate: "03-05, April 2024",
-        platform: "Prime Video",
-    },
+import axios from 'axios';
 
-
-
-];
 export default function Watchlog() {
+    const content = [
+        {
+            title: "Money Heist : Berlin",
+            genre: "",
+            director: "Álex Pina",
+            watchDate: "14-, April, 2024",
+            platform: "Netflix"
+        },
+        {
+            title: "Amar Singh Chamikla",
+            genre: "Biopic, Drama, Musical",
+            director: "Imtiaz Ali",
+            watchDate: "13, April 2024",
+            platform: "Netflix",
+        },
+        {
+            title: "Murder Mubarak",
+            genre: "Comedy, Mystery, Horror",
+            director: "Homi Adajania",
+            watchDate: "06-07, April 2024",
+            platform: "Netflix",
+        },
+        {
+            title: "Farzi",
+            genre: "Black comedy, Crime thriller",
+            director: "Raj & DK",
+            watchDate: "03-05, April 2024",
+            platform: "Prime Video",
+        },
+
+
+
+
+    ];
+
+    useEffect(() => {
+        const data = axios.get("https://cors.yasbr.com/https://api.notion.com/v1/databases/8a6b7ddb2dfb40c6a216cfa34f1a8f1d",
+            {
+                headers: {
+                    "Authorization": `Bearer secret_e5dEl4RGdNjseQingBfJ9g51Juiky8ONGVQMyvq7GeZ`,
+                    "Notion-Version": "2022-06-28",
+                    "Content-Type": "application/json"
+                },
+            })
+    }, [content])
     return (
         <div className="h-32 max-h-full md:max-h-screen">
             <div className="flex flex-col h-screen justify-between">
@@ -43,7 +64,7 @@ export default function Watchlog() {
                                     <th className='border p-4'>Genre</th>
                                     <th className='border p-4'>Director</th>
                                     <th className='border p-4'>Watched</th>
-                                    <th className='border p-4'>Platform</th>
+                                    {/* <th className='border p-4'>Platform</th> */}
                                 </tr>
                             </thead>
                             <tbody>
@@ -54,7 +75,7 @@ export default function Watchlog() {
                                         <td className='border p-4'>{movie.genre}</td>
                                         <td className='border p-4'>{movie.director}</td>
                                         <td className='border p-4'>{movie.watchDate}</td>
-                                        <td className='border p-4'>{movie.platform}</td>
+                                        {/* <td className='border p-4'>{movie.platform}</td> */}
                                     </tr>
                                 ))}
                             </tbody>
