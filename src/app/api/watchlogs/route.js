1033451c512064f6d9938aea33400aa598f1b9b4
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-08-23 19:35:07 +0530

    Watchlogs connected to notion

--- src/app/api/watchlogs/route.js
+++ src/app/api/watchlogs/route.js
@@ -4,7 +4,12 @@ export async function GET() {
   try {
     const data = await getWatchlogs();
     if (data) {
-      return Response.json(data);
+      return NextResponse.json(data, {
+        status: 200,
+        headers: {
+          "Cache-Control": "no-store, max-age=0",
+        },
+      });
     } else {
       return Response.json({ error: "Failed to fetch data from Notion" });
     }
