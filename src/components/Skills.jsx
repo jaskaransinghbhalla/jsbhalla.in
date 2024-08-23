e6b8d6e41db4f37f7230ee5242af02f0845df994
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-08-23 17:24:02 +0530

    updated to nextjs

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,9 +5,9 @@ export default function Skills() {
     <div className="my-6 text-justify">
       <div className="text-2xl font-bold">My Skills</div>
       <div className="my-6">
-        Through college courses and self-learning, I've built a solid foundation
-        in computer science and web development. Here are some technologies I'm
-        familiar with:
+        Through college courses and self-learning, I&apos;ve built a solid
+        foundation in computer science and web development. Here are some
+        technologies I&apos;m familiar with:
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 mx-4 my-3 font-medium ">
         <ul className="list-disc">
