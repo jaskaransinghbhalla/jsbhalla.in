57b7cfa54b50e6b93a1dc3c35717bee38a09613c
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-11-06 19:34:44 +0530

    Meta deta updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import NavBar from "@/components/layout/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "JS Bhalla",
+  title: "Jaskaran Singh Bhalla",
   description:
     "Jaskaran Singh Bhalla's personal website. A final year student at IIT Delhi, interested in Computer Science and Entrepreneurship. i like to build things. I hail from Ludhiana and am currently based in New Delhi.",
 };
