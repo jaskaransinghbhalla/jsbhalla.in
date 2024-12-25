e3e9956d37813dcfd8750b988845184d291ac659
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-12-25 15:16:40 +0530

    commented out cv page

--- src/app/cv/cv.jsx
+++ src/app/cv/cv.jsx
@@ -1,9 +1,9 @@
-"use client";
 import pdfViewer from "../../lib/pdfviewer";
 const CVPage = ({ device }) => {
-  if (device === "mobile") {
-    return pdfViewer({ url: "/cv.pdf", numPages: 1, width: 320 });
-  }
-  return pdfViewer({ url: "/cv.pdf", numPages: 1 });
+  // if (device === "mobile") {
+  //   return pdfViewer({ url: "/cv.pdf", numPages: 1, width: 320 });
+  // }
+  // return pdfViewer({ url: "/cv.pdf", numPages: 1 });
+  return <div>cv</div>;
 };
 export default CVPage;
