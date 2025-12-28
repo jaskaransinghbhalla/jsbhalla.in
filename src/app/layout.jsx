61d9039739770819c6aac333c916346b8f9b0d49
Jaskaran Singh Bhalla <jsbhalla.w@gmail.com>
2025-12-29 02:55:17 +0530

    add vercel analytics
    Signed-off-by: Jaskaran Singh Bhalla <jsbhalla.w@gmail.com>

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import "../../globals.css";
 import NavBar from "../components/layout/navbar";
+import { Analytics } from "@vercel/analytics/next"
+
 export const metadata = {
   title: "Jaskaran Singh Bhalla",
   description:
@@ -11,6 +13,7 @@ export default async function RootLayout({ children }) {
     <html lang="en" className="h-full">
       <body className={` flex flex-col min-h-screen`}>
           <NavBar />
+          <Analytics/>
           <div className="flex-grow">
             <div className="h-32 max-h-full md:max-h-screen">
               <div className="flex flex-col h-screen justify-between">
